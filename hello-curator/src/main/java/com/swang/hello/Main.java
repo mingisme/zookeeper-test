6f9be4de8cb0598e3f9949b2730940777d2aeadc
ming.wang4 <ming.wang4@envision-digital.com>
2023-02-27 21:10:57 +0800

    hello curator

---
+++ hello-curator/src/main/java/com/swang/hello/Main.java
@@ -0,0 +1,29 @@
+package com.swang.hello;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        String zookeeperConnectionString = "127.0.0.1:2181";
+        CuratorFramework client = CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy);
+        client.start();
+
+        if (client.checkExists().forPath("/hello-curator") == null) {
+            String value = client.create().forPath("/hello-curator", "hello curator".getBytes());
+            System.out.println(value);
+        }else{
+            byte[] bytes = client.getData().forPath("/hello-curator");
+            System.out.println("value of /hello-curator: " + new String(bytes));
+        }
+
+        List<String> children = client.getChildren().forPath("/");
+        System.out.println(children);
+    }
+}
