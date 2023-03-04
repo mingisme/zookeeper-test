3ea6b847477ff1f38adf43cbe09f55c3077e876b
ming.wang4 <ming.wang4@envision-digital.com>
2023-03-04 20:08:06 +0800

    watch

--- hello-curator/src/main/java/com/swang/basic/watch/Main.java
+++ hello-curator/src/main/java/com/swang/basic/watch/Main.java
@@ -1,4 +1,4 @@
-package com.swang.basic.config;
+package com.swang.basic.watch;
 
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
@@ -7,6 +7,7 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.x.async.AsyncCuratorFramework;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
         String zookeeperConnectionString = "127.0.0.1:2181";
@@ -15,10 +16,17 @@ public class Main {
 
         AsyncCuratorFramework async = AsyncCuratorFramework.wrap(client);
 
-        String s1 = "/singlesetting1";
-        async.create().forPath(s1);
-        async.setData().forPath(s1, "hello".getBytes());
-        async.getData().forPath(s1).thenAccept(s-> System.out.println("config value is:" + new String(s)));
+        String s = "/dev/env";
+        client.create().creatingParentsIfNeeded().forPath(s);
+        async.watched().getData().forPath(s).event().thenAccept(v->{
+            try {
+                System.out.println("value of env is: " + new String(client.getData().forPath(s)));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        async.setData().forPath(s,"product".getBytes());
 
         System.in.read();
     }
